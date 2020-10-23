6cccb4b939ec72c79a11357fc56e6b70b6c395d3
Sippora04 <sippora.toppo95@gmail.com>
2020-10-24 01:45:41 +0530

    Added: Num of times dice rolled

---
+++ SnakeAndLadder.java
@@ -0,0 +1,40 @@
+public class SnakeAndLadder {
+	public static final int startPosition = 0;
+	public static final int minPosition = 0;
+	public static final int maxPosition = 100;
+	public static final int noPlay=1;
+	public static final int ladder=2;
+	public static final int snake=3;
+	
+	public static void main(String[] args) {
+		
+		int playerPosition = startPosition;
+		int numOfDiceRolls = 0;
+		
+		System.out.println("Starting position of a Player is " + startPosition);
+		
+		while(playerPosition < 100) {	
+			int diceRoll = ((int)Math.floor(Math.random() * 10) % 6) + 1;
+			numOfDiceRolls++;
+			System.out.println("Rolling Dice: " + diceRoll);
+			int choice = (int)Math.floor(Math.random() * 10) % 3 + 1;
+			switch (choice) {
+				case noPlay: System.out.println("No Play");
+					 		 	 break;
+				case ladder: System.out.println("Ladder is there");
+							    if(playerPosition + diceRoll > maxPosition)
+							 	 System.out.println("Current Player Position: " + playerPosition);
+							 	 playerPosition = (playerPosition + diceRoll) <= maxPosition ? (playerPosition + diceRoll) : playerPosition;
+							 	 break;
+				case snake:  System.out.println("Snake is there");
+							 	 playerPosition = (playerPosition - diceRoll) >= minPosition ? (playerPosition - diceRoll) : minPosition;
+							 	 if(playerPosition == 0)					
+							 	 System.out.println("Start from 0 position.");
+							 	 break;
+				default:
+			}
+			System.out.println("Latest Player Position is " + playerPosition);
+		}
+		System.out.println("Dice Rolled: " + numOfDiceRolls + " times to win the game.");
+	}
+}
