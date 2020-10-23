997017ef5ebb790c512150fe7584877ca5c3a1cd
SIPPORA TOPPO <64629917+Sippora04@users.noreply.github.com>
2020-10-24 01:18:22 +0530

    Updated: class name

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,4 +1,4 @@
-public class SnakeLadder {
+public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int startPosition = 0;
 		System.out.println("Welcome to Snake and Ladder Simulator");
