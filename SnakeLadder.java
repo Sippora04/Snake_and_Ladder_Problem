3c37f0e934ad7b62feba3592b70b76ada21015bb
Sippora04 <sippora.toppo95@gmail.com>
2020-10-24 01:01:38 +0530

    Added: while loop condition

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,32 @@
-public class SnakeLadder {
+public class SnakeAndLadder {
+	public static final int startPosition = 0;
+	public static final int minPosition = 0;
+	public static final int noPlay=1;
+	public static final int ladder=2;
+	public static final int snake=3;
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder Simulator");
+		
+		int playerPosition = startPosition;
+		System.out.println("Starting position of a Player is " + startPosition);
+		
+		while(playerPosition < 100) {	
+			int diceRoll = ((int)Math.floor(Math.random() * 10) % 6) + 1;
+			System.out.println("Rolling Dice: " + diceRoll);
+		
+			int choice = (int)Math.floor(Math.random() * 10) % 3 + 1;
+			switch (choice) {
+				case noPlay: System.out.println("No Play");
+					 		 break;
+				case ladder: System.out.println("Ladder is there");
+							 playerPosition += diceRoll;
+							 break;
+				case snake:  playerPosition = (playerPosition - diceRoll) >= minPosition ? (playerPosition - diceRoll) : minPosition;
+							 if(playerPosition == 0)					
+							 System.out.println("Start from 0 position.");
+				default:
+			}
+			System.out.println("Latest Player Position is " + playerPosition);
+		}
 	}
 }
