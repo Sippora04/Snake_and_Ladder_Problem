01b0c78b71f05c1a65b8f0468e7539d7eb7a217d
Sippora04 <sippora.toppo95@gmail.com>
2020-10-23 23:47:15 +0530

    Added: Position of player

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,8 @@
 public class SnakeLadder {
 	public static void main(String[] args) {
+		int startPosition = 0;
 		System.out.println("Welcome to Snake and Ladder Simulator");
+		System.out.println("Starting position of a Player is " + startPosition);
+	
 	}
 }
